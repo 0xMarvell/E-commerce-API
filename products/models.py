38d2d22e49cc69e45641de40a084c690b5c2d9b4
Marvellous-Chimaraoke <rokemarvellous@gmail.com>
2022-03-26 22:20:53 +0100

    built out Headphones DB model

--- products/models.py
+++ products/models.py
@@ -25,3 +25,20 @@ class Books(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Headphones(models.Model):
+    product_tag = models.CharField(max_length=50)
+    name = models.CharField(max_length=500)
+    category = models.ForeignKey(Category, related_name='headphones', on_delete=models.CASCADE)
+    price = models.IntegerField()
+    stock = models.IntegerField()
+    image_url = models.URLField()
+    status = models.BooleanField(default=True)
+    created_on = models.DateField(auto_now_add=True)
+
+    class Meta:
+        ordering = '-created_on'
+
+    def __str__(self):
+        return f'{self.product_tag} {self.name}'
