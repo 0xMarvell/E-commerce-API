a7431f9507079748e89dfdce085c0f5e70d5b2ec
Marvellous-Chimaraoke <rokemarvellous@gmail.com>
2022-03-26 19:40:34 +0100

    registered django rest framework in settings file

--- core/settings.py
+++ core/settings.py
@@ -32,12 +32,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'cart',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # DRF
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
