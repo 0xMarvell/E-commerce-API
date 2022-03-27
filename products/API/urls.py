90413ca28c264a511344603c70b29226ef498065
Marvellous-Chimaraoke <rokemarvellous@gmail.com>
2022-03-27 18:49:25 +0100

    created url paths for all existing views

--- products/API/urls.py
+++ products/API/urls.py
@@ -1,5 +1,13 @@
 from django.urls import path
+from .views import *
 
 urlpatterns = [
+    path('categories', ListCategory.as_view(), name='categories'),
+    path('categories/<int:pk>/', DetailCategory.as_view(), name='category'),
 
+    path('books', ListBooks.as_view(), name='books'),
+    path('books/<int:pk>/', DetailBooks.as_view(), name='book'),
+
+    path('headphones', ListHeadphones.as_view(), name='headphones'),
+    path('headphones/<int:pk>/', DetailHeadphones.as_view(), name='headphone'),
 ]
