9a96071a57d15f1fff0fed971ac209ca49cf1174
Marvellous-Chimaraoke <rokemarvellous@gmail.com>
2022-03-27 18:43:23 +0100

    built API views for Headphones and Books models

--- products/API/views.py
+++ products/API/views.py
@@ -10,7 +10,24 @@ class ListCategory(generics.ListCreateAPIView):
 
 class DetailCategory(generics.RetrieveUpdateDestroyAPIView):
     queryset = Category.objects.all()
-    serializer_class = CategorySerializer   
+    serializer_class = CategorySerializer
 
 
- 
+class ListBooks(generics.ListCreateAPIView):
+    queryset = Books.objects.all()
+    serializer_class = BooksSerializer
+
+
+class DetailBooks(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Books.objects.all()
+    serializer_class = BooksSerializer 
+
+
+class ListHeadphones(generics.ListCreateAPIView):
+    queryset = Headphones.objects.all()
+    serializer_class = HeadphonesSerializer
+
+
+class DetailHeadphones(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Headphones.objects.all()
+    serializer_class = HeadphonesSerializer 
